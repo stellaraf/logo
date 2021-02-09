b4991c25f1abd3bbe9093ac3d69e39bbe9c7d6ea
checktheroads <matt@stellar.tech>
2021-02-08 18:16:50 -0700

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+<div align="center">
+  <br/>
+  <img src="https://res.cloudinary.com/stellaraf/image/upload/v1604277355/stellar-logo-gradient.svg" width="300" />
+  <br/>
+  <h3>Stellar Logo React Component</h3>
+  <br/>
+  <a href="https://github.com/stellaraf/logo/actions?query=workflow%3ATests">
+    <img src="https://img.shields.io/github/workflow/status/stellaraf/logo/Tests?color=%239100fa&event=push&style=for-the-badge" />
+  </a>
+  <br/>
+  <br/>
+</div>
+
---
+++ src/types.ts
@@ -0,0 +1,38 @@
+import type { SVGMotionProps } from 'framer-motion';
+
+export interface StellarLogoProps extends SVGMotionProps<SVGSVGElement> {
+  /**
+   * Show the "TECHNOLOGIES" block below the logo.
+   *
+   * @defaultValue `false`
+   */
+  showTagline?: boolean;
+  /**
+   * Show the ® symbol after the logo.
+   *
+   * @defaultValue `false`
+   */
+  showReserved?: boolean;
+  /**
+   * Don't animate the star.
+   *
+   * @defaultValue `false`
+   */
+  noAnimate?: boolean;
+  /**
+   * Control the color mode of the logo. In light mode, the color will be a gradient of Stellar's
+   * primary colors. In dark mode, the color will be set to `currentColor`.
+   *
+   * @defaultValue `'light'`
+   */
+  colorMode?: 'light' | 'dark';
+  /**
+   * Set the size of the logo.
+   */
+  size?: string | number;
+}
+
+export type StellarColorsType = {
+  blue: string;
+  purple: string;
+};
